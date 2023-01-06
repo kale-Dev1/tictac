f4ae0f2cd5c6f1cd6f942570833585a6cd99ce62
Kale Kamara <102758341+kale-Dev1@users.noreply.github.com>
2023-01-06 04:58:03 +0000

    Add function to place marks when clicked

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 const X_CLASS = 'x';
 const CIRCLE_CLASS = 'circle'
 const cellElements = document.querySelectorAll('[data-cell]');
-let circleTurn
+let circleTurn 
 
 cellElements.forEach(cell =>{
     cell.addEventListener('click', handleClick, {once:true})
 });
 
 function handleClick(e){
-    const cell = e.target
+    const cell = e.target;
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
+    placeMark(cell, currentClass)
     // PlaceMark
     // Check for Win
     // Check for draw
     // Switch Turns
+    swapTurns()
+    setBoardHoverClass()
 
+}
+
+function placeMark(cell, currentClass){
+    cell.classList.add(currentClass);
 }
